fdbce9eedae04843684c74e141b6cfa3385ceede
Smarch Poonkwan <smarchpoo@mathregit.group>
2020-08-14 11:08:40 +0700

    [Edit] CounrtyPackage Post Date [Edit] appModule httpClient

--- front-end/Travel-Insurance/src/app/country-package/country-package.component.ts
+++ front-end/Travel-Insurance/src/app/country-package/country-package.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-
+import { DateService } from 'src/app/services/date.service';
+import { Date } from '../model/date';
 @Component({
   selector: 'app-country-package',
   templateUrl: './country-package.component.html',
@@ -12,7 +13,7 @@ export class CountryPackageComponent implements OnInit {
   date : FormGroup;
   cap : string[] = ["Thai","England","Japan"];
   pack : string[] =["Ergonomic Metal Tuna","Gorgeous Soft Bacon","Toys"]
-  constructor(private  fb:FormBuilder) { }
+  constructor(private  fb:FormBuilder, private dateInject:DateService) { }
 
   ngOnInit(): void {
     this.date =  this.fb.group({
@@ -24,12 +25,16 @@ export class CountryPackageComponent implements OnInit {
       return this.date.controls
   }
   saveDate(){
-    const no = new Date(this.date.controls.startDate.value)
 
-     const  da  =  this.date.controls.startDate.value;
+    // const no = new Date(this.date.controls.startDate.value)
+    // let date = new Date();
+     const  start_date:string  =  this.date.controls.startDate.value;
+     const  end_date:string  =  this.date.controls.endDate.value;
+     const date = new Date(start_date, end_date)
+   this.dateInject.postDate(date).subscribe(date=>{
+       console.log("date Done")
+    })
 
-
-     console.log(da)
   }
   packageDetail(){
     this.status =true;
