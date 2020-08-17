0482c4de856f356388a00190323b8ee3529ba3b9
Araya Siriadun <araya.siriadun@gmail.com>
2020-08-17 10:12:26 +0700

    [Created]: DateTimeController with Response

---
+++ back-end/web/src/main/java/com/travelinsurance/web/util/TravelTimeResponse.java
@@ -0,0 +1,39 @@
+package com.travelinsurance.web.util;
+
+import java.time.LocalDate;
+
+public class TravelTimeResponse {
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private long duration;
+
+    public TravelTimeResponse(LocalDate startDate, LocalDate endDate, long duration) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.duration = duration;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
