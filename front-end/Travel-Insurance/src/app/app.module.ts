9b60a08681a1bff2873b76989d6bad8e0944009c
Pamuta Keawjit <pamutakea@mathregit.group>
2020-08-17 14:47:58 +0700

    [Edited]: changed payment to TransactionDetail

--- front-end/Travel-Insurance/src/app/app.module.ts
+++ front-end/Travel-Insurance/src/app/app.module.ts
@@ -1,35 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule}   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // import {MatFormFieldModule} from '@angular/material/form-field';
-import  {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
-
-
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatNativeDateModule } from '@angular/material/core';
-import {ReactiveFormsModule}   from '@angular/forms';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { CountryPackageComponent } from './country-package/country-package.component';
-import { PaymentComponent } from './payment/payment.component';
+import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountryPackageComponent,
-    PaymentComponent
+    TransactionDetailComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,9 +45,9 @@ import { PaymentComponent } from './payment/payment.component';
     HttpClientModule,
     MatToolbarModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
