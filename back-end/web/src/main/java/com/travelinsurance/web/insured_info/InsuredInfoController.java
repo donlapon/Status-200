9d0c5c105a68483d23cd85ffec402d716d02f7f3
Araya Siriadun <araya.siriadun@gmail.com>
2020-08-18 22:34:41 +0700

    [Edited]: revise saveInsuredInfo and create 2 getInsuredInfo methods in InsuredInfoController

--- back-end/web/src/main/java/com/travelinsurance/web/insured_info/InsuredInfoController.java
+++ back-end/web/src/main/java/com/travelinsurance/web/insured_info/InsuredInfoController.java
@@ -1,8 +1,12 @@
 package com.travelinsurance.web.insured_info;
 
 import com.travelinsurance.web.util.GenerateUniqueId;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +20,8 @@ public class InsuredInfoController {
 
     @CrossOrigin
     @PostMapping("/v1/insuredInfo")
-    public void saveInsuredInfo(@RequestBody NewInsuredInfoRequest[] requests) {
+    public List<InsuredInfo> saveInsuredInfo(@RequestBody NewInsuredInfoRequest[] requests) {
+        List<String> newInsuranceNumberList = new ArrayList<String>();
         for (NewInsuredInfoRequest request : requests) {
             InsuredInfo insuredInfo = new InsuredInfo();
             insuredInfo.setDestinationCountry(request.getDestinationCountry());
@@ -33,9 +38,32 @@ public class InsuredInfoController {
             insuredInfo.setBeneficiaryName(request.getBeneficiaryName());
             insuredInfo.setPhoneNumber(request.getPhoneNumber());
             insuredInfo.setEmail(request.getEmail());
-            insuredInfo.setInsuranceNumber(GenerateUniqueId.get());
             insuredInfo.setPaymentStatus(false);
+            String newInsuranceNumber = GenerateUniqueId.get();
+            insuredInfo.setInsuranceNumber(newInsuranceNumber);
+            newInsuranceNumberList.add(newInsuranceNumber);
             insuredInfoRepository.save(insuredInfo);
         }
+        List<InsuredInfo> insuredInfoList = new ArrayList<InsuredInfo>();
+        for (String insuranceNumber : newInsuranceNumberList) {
+            insuredInfoList.add(
+                    insuredInfoRepository.findInsuranceInfoByInsuranceNumber(insuranceNumber));
+        }
+        return insuredInfoList;
+    }
+
+    @CrossOrigin
+    @GetMapping("/v1/insuredInfo/{insuranceNumber}")
+    public InsuredInfo getInsuredInfoByInsuranceNumber(@PathVariable String insuranceNumber) {
+        InsuredInfo insuredInfo =
+                insuredInfoRepository.findInsuranceInfoByInsuranceNumber(insuranceNumber);
+        return insuredInfo;
+    }
+
+    @CrossOrigin
+    @GetMapping("/v1/insuredInfo/{idNumber}")
+    public InsuredInfo getInsuredInfoByIdNumber(@PathVariable String idNumber) {
+        InsuredInfo insuredInfo = insuredInfoRepository.findInsuranceInfoByIdNumber(idNumber);
+        return insuredInfo;
     }
 }
