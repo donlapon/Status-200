67aeea13af5985d64309b3e4e965e219b0df27f6
Araya Siriadun <araya.siriadun@gmail.com>
2020-08-18 10:05:34 +0700

    [Edited]: Rename API in DateTimeController

--- back-end/web/src/main/java/com/travelinsurance/web/util/DateTimeController.java
+++ back-end/web/src/main/java/com/travelinsurance/web/util/DateTimeController.java
@@ -19,31 +19,20 @@ public class DateTimeController {
     final long MAX_DURATION = 180;
 
     @CrossOrigin
-    @GetMapping("/v1/current_date_and_max_arrival_date")
+    @GetMapping("/v1/currentDateAndMaxArrivalDate")
     public TravelTimeResponse getMaxDurationFromNow(
             @RequestParam(name = "zone", defaultValue = "UTC+7") String zoneId) {
         LocalDate currentDate = LocalDate.now(ZoneId.of(zoneId));
         LocalDate maxArrivalDate = currentDate.plusDays(MAX_DURATION - 1);
-        return new TravelTimeResponse(currentDate, maxArrivalDate, 180, 0);
+        return new TravelTimeResponse(currentDate, maxArrivalDate, 180, -1);
     }
 
     @CrossOrigin
-    @GetMapping("/v1/max_arrival_date")
+    @GetMapping("/v1/maxArrivalDateFromDepartureDate")
     public TravelTimeResponse getArrivalDate(
             @RequestParam(name = "departure_date") String departureDateStr) {
         LocalDate departureDate = LocalDate.parse(departureDateStr);
         LocalDate maxArrivalDate = departureDate.plusDays(MAX_DURATION - 1);
-        return new TravelTimeResponse(departureDate, maxArrivalDate, 180, 0);
-    }
-
-    private static final AtomicLong LAST_TIME_MS = new AtomicLong();
-
-    public static long uniqueCurrentTimeMS() {
-        long now = System.currentTimeMillis();
-        while (true) {
-            long lastTime = LAST_TIME_MS.get();
-            if (lastTime >= now) now = lastTime + 1;
-            if (LAST_TIME_MS.compareAndSet(lastTime, now)) return now;
-        }
+        return new TravelTimeResponse(departureDate, maxArrivalDate, 180, -1);
     }
 }
