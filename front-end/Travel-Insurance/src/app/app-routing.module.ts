7e127452e41b4704f57a1b3e5e0b1c2faf5ea81f
Pamuta Keawjit <pamutakea@mathregit.group>
2020-08-19 18:17:59 +0700

    [Edited]: edited terms and condition routing

--- front-end/Travel-Insurance/src/app/app-routing.module.ts
+++ front-end/Travel-Insurance/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { UsersDetailComponent } from './users-detail/users-detail.component';
 import { TransactionDetailComponent } from './transaction-detail/transaction-detail.component';
 import { CountryPackageComponent } from './country-package/country-package.component';
@@ -11,7 +12,7 @@ const routes: Routes = [
   { path: 'countrypackage', component: CountryPackageComponent },
   { path: 'userdetail', component: UsersDetailComponent },
   { path: 'showuserdetail', component: ShowUserDetailComponent },
-  { path: 'termsandconditions', component: TransactionDetailComponent },
+  { path: 'termsandconditions', component: TermsAndConditionsComponent },
 ];
 
 @NgModule({
