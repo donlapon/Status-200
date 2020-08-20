593bb909426d3d896fec3fa938cae074ff86b8a0
Anthika Jirattananon <anthikajir@mathregit.group>
2020-08-20 10:29:06 +0700

    [Edited] edit name data

--- front-end/Travel-Insurance/src/app/model/user-detail.ts
+++ front-end/Travel-Insurance/src/app/model/user-detail.ts
@@ -1,27 +1,27 @@
 export class UserDetail {
 
-    titleName: string;
-    firstname: string;
-    lastname: string;
+    title: string;
+    firstName: string;
+    lastName: string;
     citizenId: string;
     dateOfBirth: string;
     beneficially: string;
     numberOfTraveller: number;
     email: string;
     constructor(
-        titleName: string,
-        firstname: string,
-        lastname: string,
+        title: string,
+        firstName: string,
+        lastName: string,
         citizenId: string,
         dateOfBirth: string,
         beneficially: string,
-        email: string){
-            this.titleName = titleName;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.citizenId = citizenId;
-            this.dateOfBirth = dateOfBirth;
-            this.beneficially = beneficially;
-            this.email = email;
+        email: string) {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.citizenId = citizenId;
+        this.dateOfBirth = dateOfBirth;
+        this.beneficially = beneficially;
+        this.email = email;
     }
 }
--- front-end/Travel-Insurance/src/app/services/user.service.ts
+++ front-end/Travel-Insurance/src/app/services/user.service.ts
@@ -12,43 +12,44 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  sendInsuredInfo(insuredInfo: UserDetail): Observable<InsuredInfo[]>{
+  sendInsuredInfo(insuredInfo: UserDetail): Observable<InsuredInfo[]> {
     const dateOfBirth = '2020-08-19';
     const user = new InsuredInfo('destinationCountry',
-                              'packageName',
-                              '2020-08-18',
-                              '2020-08-18',
-                              0,
-                              0,
-                              insuredInfo.citizenId,
-                              insuredInfo.firstname,
-                              insuredInfo.lastname,
-                              insuredInfo.titleName,
-                              dateOfBirth,
-                              insuredInfo.beneficially,
-                              '',
-                              insuredInfo.email);
+      'packageName',
+      '2020-08-18',
+      '2020-08-18',
+      0,
+      0,
+      insuredInfo.citizenId,
+      insuredInfo.firstName,
+      insuredInfo.title,
+      insuredInfo.lastName,
+
+      dateOfBirth,
+      insuredInfo.beneficially,
+      '',
+      insuredInfo.email);
     console.log('ddd', user);
     return this.http.post<InsuredInfo[]>('http://167.99.70.167:8080/api/v1/insuredInfo', [user]);
-  // postUserDetail(insuredInfo): Observable<InsuredInfo[]>{
-  //  const user1 = new InsuredInfo('destinationCountry',
-  //                                 'packageName',
-  //                                 '2020-08-18',
-  //                                 '2020-08-18',
-  //                                 0,
-  //                                 0,
-  //                                 'idNumber',
-  //                                 'titleName',
-  //                                 'firstname',
-  //                                 'lastname',
-  //                                 '2020-08-18',
-  //                                 'beneficiaryName',
-  //                                 'phoneNumber',
-  //                                 'email');
-  // //  const user2 = new UserDetail('Mrs', 'llll', 'oooo', 'llllll', '12/06/1997', 'gggg llll', 1);
-  // //  const user3 = new UserDetail('mr', 'pppp', 'iiii', 'llllll', '12/06/1997', 'gggg llll', 1);
-  //  return this.http.post<InsuredInfo[]>('http://167.99.70.167:8080/api/v1/insuredInfo', user1);
+    // postUserDetail(insuredInfo): Observable<InsuredInfo[]>{
+    //  const user1 = new InsuredInfo('destinationCountry',
+    //                                 'packageName',
+    //                                 '2020-08-18',
+    //                                 '2020-08-18',
+    //                                 0,
+    //                                 0,
+    //                                 'idNumber',
+    //                                 'titleName',
+    //                                 'firstname',
+    //                                 'lastname',
+    //                                 '2020-08-18',
+    //                                 'beneficiaryName',
+    //                                 'phoneNumber',
+    //                                 'email');
+    // //  const user2 = new UserDetail('Mrs', 'llll', 'oooo', 'llllll', '12/06/1997', 'gggg llll', 1);
+    // //  const user3 = new UserDetail('mr', 'pppp', 'iiii', 'llllll', '12/06/1997', 'gggg llll', 1);
+    //  return this.http.post<InsuredInfo[]>('http://167.99.70.167:8080/api/v1/insuredInfo', user1);
 
-  // //  return of([user1, user2, user3]);
+    // //  return of([user1, user2, user3]);
   }
 }
